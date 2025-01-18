f57a5e7c5f37d9bcc2bfaadecb8072990bb121cd
EmaniBoy <karthikemani0321@gmail.com>
2025-01-18 15:56:56 -0600

    hangman

---
+++ Hangman.java
@@ -0,0 +1,64 @@
+import java.util.Scanner;
+import java.util.Random;
+import java.util.Arrays;
+
+public class Hangman {
+    public static void main(String[] args) {
+        String words[] = {"programming", "arrays", "hangman", "science", "java", "computer"};
+        Random random = new Random();
+        String word = words[random.nextInt(words.length)];
+        char[] hiddenWord = new char[word.length()];
+        Arrays.fill(hiddenWord, '_');
+        int incorrectGuesses = 0;
+        Scanner keyboard = new Scanner(System.in);
+
+        String[] hangmanArt = {
+            "  +---+\n  |   |\n      |\n      |\n      |\n      |\n=========",
+            "  +---+\n  |   |\n  O   |\n      |\n      |\n      |\n=========",
+            "  +---+\n  |   |\n  O   |\n  |   |\n      |\n      |\n=========",
+            "  +---+\n  |   |\n  O   |\n /|   |\n      |\n      |\n=========",
+            "  +---+\n  |   |\n  O   |\n /|\\  |\n      |\n      |\n=========",
+            "  +---+\n  |   |\n  O   |\n /|\\  |\n /    |\n      |\n=========",
+            "  +---+\n  |   |\n  O   |\n /|\\  |\n / \\  |\n      |\n========="
+        };
+
+        while(true){
+            System.out.println(hangmanArt[incorrectGuesses]);
+            System.out.println("Word: " + new String(hiddenWord));
+            System.out.println("Guess a letter: ");
+            char guess = keyboard.next().charAt(0);
+
+            if (!Character.isLetter(guess)) {
+                System.out.println("Please enter a valid letter.");
+                continue;
+            }
+
+
+            boolean correctGuess = false;
+            for(int i = 0; i < word.length(); i++){
+                if(word.charAt(i) == guess){
+                    hiddenWord[i] = guess;
+                    correctGuess = true;
+                }
+            }
+
+            if(!correctGuess){
+                incorrectGuesses++;
+            }
+
+
+            if(new String(hiddenWord).equals(word)){
+                System.out.println("congratulations, you guessed the word: " + word);
+                break;
+            }
+
+            if (incorrectGuesses >= 6) {
+                System.out.println(hangmanArt[incorrectGuesses]);
+                System.out.println("You lost! The word was: " + word);
+                break;
+            }
+        }
+        keyboard.close();
+
+    }
+}

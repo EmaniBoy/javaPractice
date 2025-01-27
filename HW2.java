e0ee194e15a23ac489c6fdba9a99bc1d0d5756b2
EmaniBoy <karthikemani0321@gmail.com>
2025-01-27 15:40:35 -0600

    HW2.2

--- HW2.java
+++ HW2.java
@@ -5,16 +5,17 @@ public class HW2 {
     public static void main(String[] args) {
         Scanner keyboard = new Scanner(System.in);
         Random random = new Random();
-        int randomInt = random.nextInt(3) + 1;
         int rounds = 3;
         int compScore = 0;
         int userScore = 0;
-        String compChoice = "";
+        
 
         
         
         System.out.println("Welcome to Rock, Paper, Scissors game!");
         for(int i = 1; i <= rounds; i++){
+            int randomInt = random.nextInt(3) + 1;
+            String compChoice = "";
             if(randomInt == 1){
                 compChoice = "ROCK";
             } else if(randomInt == 2) {
@@ -22,7 +23,6 @@ public class HW2 {
             } else if(randomInt == 3){
                 compChoice = "SCISSORS";
             }
-
 
             System.out.println("round: " + i);
             System.out.println("Please enter Rock, Paper, or Scissors: ");
@@ -37,8 +37,24 @@ public class HW2 {
                         (userChoice.equals("SCISSORS") && compChoice.equals("PAPER"))){
                             System.out.println("You win this round!");
                             userScore++;
+            } else {
+                System.out.println("Computer wins this round");
+                compScore++;
             }
         }
+        System.out.println("Final Scores: ");
+        System.out.println("You: " + userScore);
+        System.out.println("Compute: " + compScore);
+
+        if(userScore > compScore){
+            System.out.println("You win this game!");
+        } else if(userScore == compScore){
+            System.out.println("This game is a tie!");
+        } else {
+            System.out.println("You lose this game!");
+        }
+
+        keyboard.close();
 
     }
     

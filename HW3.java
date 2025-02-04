750e95985c7fd294e4c6f28cf98aae9a0d3add49
EmaniBoy <karthikemani0321@gmail.com>
2025-02-04 14:08:12 -0600

    HW3

---
+++ HW3.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.ArrayList;
+
+public class HW3 {
+    public static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in);
+        ArrayList<String> map = new ArrayList<String>();
+        System.out.println("hello world");
+    }
+}   
